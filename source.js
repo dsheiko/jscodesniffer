3b4a85e4e2cc9d148e7464cdb891a64bcd5a7146
Dmitry Sheiko <web.sbelarus@gmail.com>
2012-12-18 15:14:01 +0100

    output color theme improved

--- source.js
+++ source.js
@@ -1,5 +1,20 @@
+/**
+* @package JSA.dbc
+* @author sheiko
+* @version 2.03
+* @license MIT
+* @copyright (c) Dmitry Sheiko http://www.dsheiko.com
+* Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
+*/
 
-foo({
-  a: "alpha",
-  b: "beta"
-});
+function   ThisIs7InvalidId()
+{
+    var i = ( 7 + (8 - 9) ), a = function() {
+        return "**********";
+    };
+    if ( true ) {
+
+    }
+
+}
+
