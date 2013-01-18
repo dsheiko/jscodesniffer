e9bde3db697f95d6a9f06c35cede656ec784beb2
Dmitry Sheiko <web.sbelarus@gmail.com>
2013-01-18 17:06:12 +0100

    IdentifierNaming sniff now allows names like  which whidely used to mark variables keeping jQuery instances

--- standard/AbstractIdiomatic.js
+++ standard/AbstractIdiomatic.js
@@ -41,7 +41,7 @@ var AbstractStandard = require('./AbstractStandard'),
             var current = tokenizer.current(),
                 // checks for cameCase or PascalCase
                 isValidIdentifierName = function( string ) {
-                    var validRe = /^_*[a-zA-Z]*[0-9]*$/,
+                    var validRe = /^_*\$*[a-zA-Z]*[0-9]*$/,
                         isConstantRe = /^[A-Z_]+[0-9]*$/,
                         noUcRepRe = /[A-Z]{2}/;
                     if ( string === "$" ) {
