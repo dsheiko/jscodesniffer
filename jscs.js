75e95cf1043c5c95df3ce2739f2c847ea07dd5f8
Dmitry Sheiko <web.sbelarus@gmail.com>
2013-12-20 12:22:39 +0100

    tokens and comments removed from SyntaxTree analysis

--- jscs.js
+++ jscs.js
@@ -75,7 +75,7 @@ var MIN_REPORT_WIDTH = 32,
         dictionary = new Dictionary();
 
         rulesetOverrides = cli.readRealtimeConfig( process.cwd() );
-        
+
         cli.applyToEveryFileInPath( where , function( pathArg, data ) {
             logger = sniffer.getTestResults( data, options, rulesetOverrides );
             reporter.add( pathArg, dictionary.translateBulk( logger.getMessages() ), options.standard );
@@ -97,9 +97,9 @@ var MIN_REPORT_WIDTH = 32,
           process.exit( 1 );
         }
       };
-main();
-//try {
-// main();
-//} catch ( e ) {
-//  console.error( e );
-//}
+
+try {
+ main();
+} catch ( e ) {
+  console.error( e );
+}
