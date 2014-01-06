36dd50e228eab8393877db05ef554194e2a621d7
Dmitry Sheiko <web.sbelarus@gmail.com>
2014-01-06 13:49:50 +0100

    Meeting requirements of jshint JQuery standard settings

--- standard/Jquery.js
+++ standard/Jquery.js
@@ -1,22 +1,22 @@
 /*
- * @package jscodesniffer
- * @author sheiko
- * @license MIT
- * @copyright (c) Dmitry Sheiko http://www.dsheiko.com
- * @jscs standard:Jquery
- * Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
- */
+  * @package jscodesniffer
+  * @author sheiko
+  * @license MIT
+  * @copyright (c) Dmitry Sheiko http://www.dsheiko.com
+  * @jscs standard:Jquery
+  * Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
+  */
 
 // UMD boilerplate according to https://github.com/umdjs/umd
-if ( typeof module === 'object' && typeof define !== 'function' ) {
+if ( typeof module === "object" && typeof define !== "function" ) {
     var define = function ( factory ) {
         module.exports = factory( require, exports, module );
     };
 }
 define( function ( require, exports, module ) {
   /**
-   * Rules based on contribute.jquery.org/style-guide/js/
-   */
+    * Rules based on contribute.jquery.org/style-guide/js/
+    */
   return {
   /*
     Indentation with tabs.
@@ -33,13 +33,13 @@ define( function ( require, exports, module ) {
     },
   /*
     Lines should be no longer than 80 characters
-   */
+    */
     "LineLength": {
       "allowMaxLength": 80
     },
   /*
     Any , and ; must not have preceding space.
-   */
+    */
     "CommaPunctuatorSpacing": {
       "disallowPrecedingSpaces": false
     },
@@ -47,8 +47,8 @@ define( function ( require, exports, module ) {
       "disallowPrecedingSpaces": false
     },
     /*
-     if/else/for/while/try always have braces and always go on multiple lines.
-     */
+      if/else/for/while/try always have braces and always go on multiple lines.
+      */
     "CompoundStatementConventions": {
       "for": [
         "IfStatement",
@@ -64,14 +64,14 @@ define( function ( require, exports, module ) {
       "requireMultipleLines": true
     },
     /*
-     Unary special-character operators (e.g., !, ++) must not have space next to their operand.
-     */
+      Unary special-character operators (e.g., !, ++) must not have space next to their operand.
+      */
     "UnaryExpressionIdentifierSpacing": {
       "allowTrailingWhitespaces" : 0
     },
     /*
-     The ? and : in a ternary conditional must have space on both sides.
-     */
+      The ? and : in a ternary conditional must have space on both sides.
+      */
     "TernaryConditionalPunctuatorsSpacing": {
       "allowTestTrailingWhitespaces": 1,
       "allowConsequentPrecedingWhitespaces": 1,
@@ -80,7 +80,7 @@ define( function ( require, exports, module ) {
     },
     /*
       No filler spaces in empty constructs (e.g., {}, [], fn())
-     */
+      */
     "EmptyConstructsSpacing": {
       "for": [
         "ObjectExpression",
@@ -102,15 +102,15 @@ define( function ( require, exports, module ) {
       "allowElementTrailingWhitespaces": 1
     },
     /*
-     jQuery uses double quotes.
-     */
+      jQuery uses double quotes.
+      */
     "QuoteConventions": {
       "allowDoubleQuotes": true,
       "allowSingleQuotes": false
     },
     /*
-     Variable and function names should be full words, using camel case with a lowercase first letter.
-     */
+      Variable and function names should be full words, using camel case with a lowercase first letter.
+      */
     "VariableNamingConventions": {
       "allowCase": ["camel"],
       "allowRepeating": true,
@@ -146,10 +146,10 @@ define( function ( require, exports, module ) {
       "allowParamTrailingWhitespaces": 1
     },
     /*
-     When a chain of method calls is too long to fit on one line, there must be one call per line,
-     with the first call on a separate line from the object the methods are called on.
-     If the method changes the context, an extra level of indentation must be used.
-     */
+      When a chain of method calls is too long to fit on one line, there must be one call per line,
+      with the first call on a separate line from the object the methods are called on.
+      If the method changes the context, an extra level of indentation must be used.
+      */
     "ChainedMethodCallsSpacing" : {
       "allowTrailingObjectWhitespaces": 0,
       "allowPrecedingPropertyWhitespaces": 0
@@ -160,9 +160,9 @@ define( function ( require, exports, module ) {
       "allowOperatorTrailingWhitespaces": 1
     },
     /*
-     Assignments in a declaration must be on their own line. Declarations that don't have an assignment
-     must be listed together at the start of the declaration
-     */
+      Assignments in a declaration must be on their own line. Declarations that don't have an assignment
+      must be listed together at the start of the declaration
+      */
     "VariableDeclarationPerScopeConventions" : {
       "disallowMultiplePerBlockScope": true
     }
