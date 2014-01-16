22c28bb68a3f073c8b52160edff877db877ff4c8
Dmitry Sheiko <web.sbelarus@gmail.com>
2014-01-16 14:26:34 +0100

    JSDoc coments improved

--- standard/Jquery.js
+++ standard/Jquery.js
@@ -177,8 +177,7 @@ define(function() {
 		must be listed together at the start of the declaration
 		*/
 	"VariableDeclarationPerScopeConventions": {
-		"disallowMultiplePerBlockScope": true,
-		"requireInTheBeginning": true
+		"disallowMultiplePerBlockScope": true
 	}
 
 	};
