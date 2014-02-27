ee760f836f7dab8f6d0d95fa1c2dfc736b5fc6bb
Dmitry Sheiko <dmitry@crytek.com>
2014-02-27 10:19:45 +0100

    fixing inline documentation

--- jscs-module.js
+++ jscs-module.js
@@ -1,12 +1,22 @@
 /*
-	* @package jscodesniffer
-	* @author sheiko
-	* @license MIT
-	* @copyright (c) Dmitry Sheiko http://www.dsheiko.com
-	* @jscs standard:Jquery
-	* jshint unused:false
-	* Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
-	*/
+* @package jscodesniffer
+* @author sheiko
+* @license MIT
+* @copyright (c) Dmitry Sheiko http://www.dsheiko.com
+* @jscs standard:Jquery
+* jshint unused:false
+* Code style: http://docs.jquery.com/JQuery_Core_Style_Guidelines
+*/
+
+/**
+* Executing jscs cli
+* @module jscs-module
+* @requires module:lib/Sniffer
+* @requires module:lib/Reporter
+* @requires module:lib/Dictionary
+* @requires module:lib/Cli
+*/
+
 // UMD boilerplate according to https://github.com/umdjs/umd
 if ( typeof module === "object" && typeof define !== "function" ) {
 	var define = function ( factory ) {
@@ -14,19 +24,69 @@ if ( typeof module === "object" && typeof define !== "function" ) {
 	};
 }
 /**
-	* Executing jscs cli
-	* @module jscodesniffer
-	*/
+* @param {function} require
+*/
 define(function( require ) {
 	"use strict";
+			/**
+			 * @constant
+			 * @default
+			 * @memberOf module:jscs-module
+			 * @type {number}
+			 * @access private
+			 */
 	var MIN_REPORT_WIDTH = 32,
+			/**
+			 * @constant
+			 * @default
+			 * @memberOf module:jscs-module
+			 * @type {number}
+			 * @access private
+			 */
 			DEFAULT_REPORT_WIDTH = 84,
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {module:lib/Sniffer}
+			 * @access private
+			 */
 			Sniffer = require( "./lib/Sniffer" ),
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {module:lib/Reporter}
+			 * @access private
+			 */
 			Reporter = require( "./lib/Reporter" ),
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {module:lib/Dictionary}
+			 * @access private
+			 */
 			Dictionary = require( "./lib/Dictionary" ),
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {module:lib/Cli}
+			 * @access private
+			 */
 			Cli = require( "./lib/Cli" ),
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {function}
+			 * @access private
+			 */
 			fs = require( "fs" ),
+			/**
+			 * @memberOf module:jscs-module
+			 * @type {function}
+			 * @access private
+			 */
 			path = require( "path" ),
+			/**
+			 * @constant
+			 * @default
+			 * @memberOf module:jscs-module
+			 * @type {number}
+			 * @access private
+			 */
 			HELP_SCREEN = "Usage: jscs <path> <path>..\n" +
 				"<path> - filename or dir to sniff\n" +
 				"[--standard=<Standard>] - apply specified standard (Idiomatic, Jquery)\n" +
@@ -40,15 +100,24 @@ define(function( require ) {
 				"[--reportWidth=" + DEFAULT_REPORT_WIDTH + "] - How many columns wide screen reports should be printed\n";
 	/**
 	* @constructor
-	* @alias module:jscodesniffer
+	* @alias module:jscs-module
 	* @param {string[]} argv
 	* @param {string} [cwd]
 	* @param {string} [srcCode]
 	*/
 	return function( argv, cwd, srcCode ) {
+	/**
+	 * @typedef optionsDto
+	 * @type {object}
+	 * @property {string} standard - name of the coding style standard, e.g. Jquery
+	 * @property {string} highlight - colorize on ot ("1" or "0")
+	 * @property {string} report - reporter name
+	 * @property {number} reportWidth - width of the report
+	 */
 		var
 			/**
 			* Default options
+			* @type {optionsDto}
 			*/
 			options = {
 				standard: null,
