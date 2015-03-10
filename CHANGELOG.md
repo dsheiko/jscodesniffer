26588380ed5b4d27831e6c0c35d6c4c22d40e7a2
Dmitry Sheiko <dmitry@crytek.com>
2015-03-10 11:33:48 +0100

    R2.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+## R2.2.0
+* New constrains of CompoundStatementConventions introduced
+  "allowOpeningBracePrecedingWhitespaces", "allowOpeningBraceTrailingWhitespaces",
+  "requireOpeningBracePrecedingNewLine", "requireOpeningBraceTrailingNewLine",
+  "allowClosingBracePrecedingWhitespaces", "requireClosingBracePrecedingNewLine"
+  See https://github.com/dsheiko/jscodesniffer/issues/17
+
+## R2.1.15
+* Support for .jscsignore. See https://github.com/dsheiko/jscodesniffer/issues/22
+* Relax indentation rule(s) for JSDoc comments. See https://github.com/dsheiko/jscodesniffer/issues/21
+
+## R2.1.14
+* ArgumetSpacing sniff - config param `for` is optional. See https://github.com/dsheiko/jscodesniffer/issues/18
--- test/int-sniff-compound-statement.js
+++ test/int-sniff-compound-statement.js
@@ -11,6 +11,10 @@ Array.prototype.hasErrorCode = function( errCode ) {
 };
 
 var OPTIONS = { standard: "Jquery"  },
+    counter = 0,
+    inc = function() {
+      return " (" + ( ++counter ) + ")";
+    },
     statements = [
           "IfStatement",
           "SwitchStatement",
@@ -22,14 +26,14 @@ var OPTIONS = { standard: "Jquery"  },
           "TryStatement"
         ];
 
-describe( "CompoundStatementConventions", function () {
+describe( "CompoundStatementConventions /allow(*)Brace(*)Whitespaces, require(*)BracePrecedingNewLine/", function () {
   var sniffer, logger = null;
   beforeEach(function(){
     sniffer = new Sniffer();
   });
 
   describe( "IfStatement", function () {
-    it(" treats  allow(*)Brace(*)Whitespaces, require(*)BracePrecedingNewLine correctly (1)", function () {
+    it(" throws no exceptions when code is valid" + inc(), function () {
      var code = "if (dumb)\n{\ndumb = 1;\n}",
       modifiers = {
         "CompoundStatementConventions": {
@@ -47,31 +51,112 @@ describe( "CompoundStatementConventions", function () {
       logger = sniffer.getTestResults( code, OPTIONS, modifiers );
       logger.getMessages().length.should.not.be.ok;
     });
-    it(" throws errors when no whitespace preceding opening brace, but one required, NL required (2)", function () {
+    it(" throws errors when no whitespace preceding opening brace, but one required, NL required" + inc(),
+    function () {
      var code = "if (dumb){\ndumb = 1;\n}",
       modifiers = {
         "CompoundStatementConventions": {
           "for": statements,
-          "requireBraces": true,
-          "requireMultipleLines": true,
           "allowOpeningBracePrecedingWhitespaces": 1,
-          "allowOpeningBraceTrailingWhitespaces": 1,
-          "requireOpeningBracePrecedingNewLine": true,
-          "requireOpeningBraceTrailingNewLine": true,
-          "allowClosingBracePrecedingWhitespaces": 1,
-          "requireClosingBracePrecedingNewLine": true
+          "requireOpeningBracePrecedingNewLine": true
         }
       };
       logger = sniffer.getTestResults( code, OPTIONS, modifiers );
-      //console.log(logger.getMessages());
       logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBracePrecedingWhitespaces" ).
         should.be.ok;
       logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBracePrecedingNewLine" ).
         should.be.ok;
-      //logger.getMessages().length.should.not.be.ok;
+
+    });
+    it(" throws errors when no LN preceding opening brace, but one required" + inc(), function () {
+       var code = "if (dumb) {\ndumb = 1;\n}",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowOpeningBracePrecedingWhitespaces": 1,
+            "requireOpeningBracePrecedingNewLine": true
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBracePrecedingNewLine" ).
+          should.be.ok;
+    });
+
+    it(" throws no errors when no LN preceding opening brace and none required" + inc(), function () {
+       var code = "if (dumb) {\ndumb = 1;\n}",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowOpeningBracePrecedingWhitespaces": 1, // OK
+            "requireOpeningBracePrecedingNewLine": false
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().length.should.not.be.ok;
+    });
+
+
+    it(" throws errors when no LN trailing opening brace, but one required" + inc(), function () {
+       var code = "if (dumb)\n{ dumb = 1;\n}",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowOpeningBraceTrailingWhitespaces": 1,
+            "requireOpeningBraceTrailingNewLine": true
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBraceTrailingNewLine" ).
+          should.be.ok;
+    });
+
+    it(" throws errors when many spaces trailing opening brace, but one required" + inc(), function () {
+       var code = "if (dumb)\n{   dumb = 1;\n}",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowOpeningBraceTrailingWhitespaces": 1
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBraceTrailingWhitespaces" ).
+          should.be.ok;
+    });
+
+    it(" throws errors when no LN preceding closing brace, but one required" + inc(), function () {
+       var code = "if (dumb)\n{ dumb = 1; }",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowClosingBracePrecedingWhitespaces": 1,
+            "requireClosingBracePrecedingNewLine": true
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().hasErrorCode( "CompoundStatementRequireClosingBracePrecedingNewLine" ).
+          should.be.ok;
+    });
+
+    it(" throws errors when many spaces preceding closing brace, but one required" + inc(), function () {
+       var code = "if (dumb)\n{ dumb = 1;   }",
+        modifiers = {
+          "CompoundStatementConventions": {
+            "for": statements,
+            "allowClosingBracePrecedingWhitespaces": 1
+          }
+        };
+        logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+        logger.getMessages().hasErrorCode( "CompoundStatementAllowClosingBracePrecedingWhitespaces" ).
+          should.be.ok;
+    });
   });
-  it(" throws errors when no LN preceding opening brace, but one required (3)", function () {
-     var code = "if (dumb) {\ndumb = 1;\n}",
+
+
+
+
+  describe( "ForStatement", function () {
+    it(" throws no exceptions when code is valid" + inc(), function () {
+     var code = "for ( i = 1; i < 10; i++ )\n{\na = 2;\n}",
       modifiers = {
         "CompoundStatementConventions": {
           "for": statements,
@@ -86,11 +171,133 @@ describe( "CompoundStatementConventions", function () {
         }
       };
       logger = sniffer.getTestResults( code, OPTIONS, modifiers );
-      //console.log(logger.getMessages());
+      logger.getMessages().length.should.not.be.ok;
+    });
 
+    it(" throws errors when no whitespace preceding opening brace, but one required, NL required" + inc(),
+    function () {
+     var code = "for ( i = 1; i < 10; i++ ){\na = 2;\n}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowOpeningBracePrecedingWhitespaces": 1,
+          "requireOpeningBracePrecedingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBracePrecedingWhitespaces" ).
+        should.be.ok;
       logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBracePrecedingNewLine" ).
         should.be.ok;
-      //logger.getMessages().length.should.not.be.ok;
     });
+
+    it(" throws errors when no whitespace trailing opening brace, but one required, NL required" + inc(),
+    function () {
+     var code = "for ( i = 1; i < 10; i++ )\n{a = 2;\n}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowOpeningBraceTrailingWhitespaces": 1,
+          "requireOpeningBraceTrailingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBraceTrailingWhitespaces" ).
+        should.be.ok;
+      logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBraceTrailingNewLine" ).
+        should.be.ok;
+    });
+
+    it(" throws errors when no whitespace preceding closing brace, but one required, NL required" + inc(),
+    function () {
+     var code = "for ( i = 1; i < 10; i++ )\n{\na = 2;}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowClosingBracePrecedingWhitespaces": 1,
+          "requireClosingBracePrecedingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowClosingBracePrecedingWhitespaces" ).
+        should.be.ok;
+      logger.getMessages().hasErrorCode( "CompoundStatementRequireClosingBracePrecedingNewLine" ).
+        should.be.ok;
+    });
+
   });
+
+  describe( "TryStatement", function () {
+    it(" throws no exceptions when code is valid" + inc(), function () {
+     var code = "try\n{\na = 1;\n} catch( err ){ a = 1}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "requireBraces": true,
+          "requireMultipleLines": true,
+          "allowOpeningBracePrecedingWhitespaces": 1,
+          "allowOpeningBraceTrailingWhitespaces": 1,
+          "requireOpeningBracePrecedingNewLine": true,
+          "requireOpeningBraceTrailingNewLine": true,
+          "allowClosingBracePrecedingWhitespaces": 1,
+          "requireClosingBracePrecedingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().length.should.not.be.ok;
+    });
+
+    it(" throws errors when no whitespace preceding opening brace, but one required, NL required" + inc(),
+    function () {
+     var code = "try{\na = 1;\n} catch( err ){ a = 1}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowOpeningBracePrecedingWhitespaces": 1,
+          "requireOpeningBracePrecedingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBracePrecedingWhitespaces" ).
+        should.be.ok;
+      logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBracePrecedingNewLine" ).
+        should.be.ok;
+    });
+
+    it(" throws errors when no whitespace trailing opening brace, but one required, NL required" + inc(),
+    function () {
+     var code = "try\n{a = 1;\n} catch( err ){ a = 1}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowOpeningBraceTrailingWhitespaces": 1,
+          "requireOpeningBraceTrailingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowOpeningBraceTrailingWhitespaces" ).
+        should.be.ok;
+      logger.getMessages().hasErrorCode( "CompoundStatementRequireOpeningBraceTrailingNewLine" ).
+        should.be.ok;
+    });
+
+    it(" throws errors when no whitespace preceding closing brace, but one required, NL required" + inc(),
+    function () {
+     var code = "try\n{\na = 1;} catch( err ){ a = 1}",
+      modifiers = {
+        "CompoundStatementConventions": {
+          "for": statements,
+          "allowClosingBracePrecedingWhitespaces": 1,
+          "requireClosingBracePrecedingNewLine": true
+        }
+      };
+      logger = sniffer.getTestResults( code, OPTIONS, modifiers );
+      logger.getMessages().hasErrorCode( "CompoundStatementAllowClosingBracePrecedingWhitespaces" ).
+        should.be.ok;
+      logger.getMessages().hasErrorCode( "CompoundStatementRequireClosingBracePrecedingNewLine" ).
+        should.be.ok;
+    });
+
+  });
+
 });
